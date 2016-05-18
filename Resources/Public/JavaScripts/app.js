482d58d0d4b42bd2ac971c959ca1106675770856
ochorocho <rothjochen@gmail.com>
2016-05-18 23:29:34 +0200

    remove tinyMCE and add foundation rte

--- Resources/Public/JavaScripts/app.js
+++ Resources/Public/JavaScripts/app.js
@@ -1,28 +1,21 @@
 $(document).ready(function () {
+
+    $('textarea').wysihtml5({
+        "font-styles": false,
+        "emphasis": true,
+        "lists": false,
+        "html": false,
+        "link": false,
+        "image": false,
+        "color": true
+    });
+
     $(document).foundation();
 
-
     $("[data-toggle=lightboxImage]").click(function() {
-        var src = $(this).attr('data-image')
+        var src = $(this).attr('data-image');
         $('#lightboxImage img').attr('src', src);
     });
 
-});
 
-
-tinymce.init({
-    selector: 'textarea',
-    menubar:false,
-    statusbar: false,
-    height: 200,
-    theme: 'modern',
-    skin : "mobile",
-    plugins: [
-        'autolink lists link charmap print preview hr',
-        'wordcount visualblocks visualchars code fullscreen',
-        'insertdatetime nonbreaking directionality',
-        'emoticons template paste textpattern'
-    ],
-    toolbar1: 'bold italic emoticons | bullist numlist | undo redo',
-    image_advtab: true,
 });
